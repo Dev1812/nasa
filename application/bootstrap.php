09d85c57997bcf9366a667b469440266077d8a77
Issaev Timur <timka_issaev@mail.ru>
2017-05-10 08:51:37 +0400

    added connection of a log file, model and controller

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -1,12 +1,13 @@
 <?php
-  require_once SITE_ROOT.'application/core/database.php';
-  require_once SITE_ROOT.'application/core/date.php';
   require_once SITE_ROOT.'application/core/model.php';
   require_once SITE_ROOT.'application/core/view.php';
-  /*require_once SITE_ROOT.'application/core/controller.php';
+  require_once SITE_ROOT.'application/core/log.php'; 
   require_once SITE_ROOT.'application/core/route.php';
-  require_once SITE_ROOT.'application/core/i18n.php';
-  require_once SITE_ROOT.'application/core/log.php';   
+  require_once SITE_ROOT.'application/core/controller.php';
+  /*require_once SITE_ROOT.'application/core/database.php';
+  require_once SITE_ROOT.'application/core/date.php';
+  require_once SITE_ROOT.'application/core/controller.php';
+  require_once SITE_ROOT.'application/core/i18n.php';  
   require_once SITE_ROOT.'application/core/user.php';
   require_once SITE_ROOT.'application/core/crypto.php';
   require_once SITE_ROOT.'application/core/common.php';
