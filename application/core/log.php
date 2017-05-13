3c9fd3e7c3d1591faad9a05f8b9f7d0a9bd6aea3
Issaev Timur <timka_issaev@mail.ru>
2017-05-13 14:37:24 +0400

    file is rewritten, structure and function write() is improved

--- application/core/log.php
+++ application/core/log.php
@@ -1,94 +1,47 @@
 <?php
 class Log{
 
-  public $date = array('day'=>00, 'month'=>00, 'year'=>0000, 'time'=>'00:00:00');
-  
-  public function __construct() {}
-  
-  /**
-   * Установка даты для открываемого файла
-   * Если день/месяц/год не переданы, то будут подставлены текущие
-   * @param <Int> day - день из названия файла
-   * @param <Int> month - месяц из названия файла
-   * @param <Int> year - год из названия файла
-   *
-   */
-  public function setDate($day, $month, $year) {
-    $today_date = date('d.m.Y');
-    $date = explode(".", $today_date);
+  private $path = null;
+  private $time = null;
 
-    $this->date['day'] = (isset($day)) ? intval($day) : $date[0];
-    $this->date['month'] = (isset($month)) ? intval($month) : $date[1];
-    $this->date['year'] = (isset($year)) ? intval($year) : $date[2];
-    $this->date['time'] = $date[3];
+  public function __construct() {
+    $date = date('d.m.Y.H:i:s');
+    list($day, $month, $year, $time) = explode('.', $date);
+
+    $this->time = $time;
+    $date_opened = $day.'.'.$month.'.'.$year;
+
+    $this->path = SITE_ROOT.'application/logs/'.$date_opened.'.txt';
   }
-
+  
+  
   /**
-   * Открытие файла для записи
-   * @return <resource> Файл для записи
-   * 
+   * Открытие Лог-файла
+   * @return <Resource> Открытый файл
+   * @date 13.05.2017
    */
-  public function openFile() {
-  	$d = $this->date;
-  	$date_opened = $d['day'].'.'. $d['month'].'.'. $d['year'];
-    $path = SITE_ROOT.'application/logs/'.$date_opened.'.txt';
-
-    $file = fopen($path, 'a+');
+  private function openLogFile() {
+    $file = fopen($this->path, 'a+');
     flock($file, LOCK_EX);
     return $file;
   }
 
   /**
-   * Получение IP пользователя
-   * @return <String> IP
-   * 
-   */
-  public function getIp() {
-    $ip = '000.0.0.0';
-    
-    if(isset($_SERVER['REMOTE_ADDR'])) {
-      $ip = $_SERVER['REMOTE_ADDR'];
-    } else if(isset($_SERVER['HTTP_CLIENT_IP'])) {
-      $ip = $_SERVER['HTTP_CLIENT_IP'];
-    }
-
-    return strip_tags($ip);
-  }
-
-
-
-  /**
-   * Запись в файл
-   * @param <String> text Строка для записи
-   * 
+   * Запись в Лог-файл
+   * @param <String> text - Текст для записи в Лог-файл
+   * @date 13.05.2017
    */
   public function write($text) {
-  	if(!isset($text)) {
-  	  return false;
-  	}
+    if(!isset($text)) return false;
 
-  	$file = $this->openFile();
-  	$ip = $this->getIp();
-  	$uid = (isset($_SESSION['uid'])) ? $_SESSION['uid'] : -1;
+    $file = self::openLogFile();
 
-  	$text_write = '['.$this->date['time'].'] '.'['.$ip.'] '.'['.$uid.'] '.$text;//[09.05.2017] [127.0.0.1] [1] Test
+    $ip = User::getIP();
+    $uid = (isset($_SESSION['uid'])) ? $_SESSION['uid'] : -1;
 
-    fwrite($file, $text);
+    $text_write = "\n[".$this->time.']'.'['.$ip.']'.'['.$uid.']'.$text;//[09.05.2017][127.0.0.1][1] Test
+    fwrite($file, $text_write);
     flock($file, LOCK_UN);
-    fclose($file);
-  }
-
-  /**
-   * Получение записей из лог-файла
-   * Если день/месяц/год не переданы, то будут подставлены текущие
-   * @param <Int> day - день из названия файла
-   * @param <Int> month - месяц из названия файла
-   * @param <Int> year - год из названия файла
-   *
-   */
-  public function get($day, $month, $year) {
-  	$file = $this->openFile($day, $month, $year);
-    var_dump($file);
     fclose($file);
   }
 
